Automated covariance/permutation invariance test fixed.

examples/train_cormorant.py
@@ -33,7 +33,7 @@ def main():
     global_cg_dict(maxl=max(args.maxl+args.max_sh), dtype=dtype, device=device)
 
     # Initialize dataloder
-    datasets, num_species, charge_scale = initialize_datasets(args.datadir, args.dataset, subset=args.subset, 
+    datasets, num_species, charge_scale = initialize_datasets(args.datadir, args.dataset, subset=args.subset,
                                                               num_pts={'train': args.num_train, 'test': args.num_test, 'valid': args.num_valid})
 
     # Construct PyTorch dataloaders from datasets
@@ -55,7 +55,7 @@ def main():
     loss_fn = torch.nn.functional.mse_loss
 
     # Apply the covariance and permutation invariance tests.
-    cormorant_tests(model, datasets['train'], args, charge_scale=charge_scale)
+    cormorant_tests(model, dataloaders['train'], args, charge_scale=charge_scale)
 
     # Instantiate the training class
     trainer = TrainCormorant(args, dataloaders, model, loss_fn, optimizer, scheduler, restart_epochs, device, dtype)

src/cormorant/data/collate.py
@@ -5,7 +5,7 @@ def batch_stack(props):
     Stack a list of torch.tensors so they are padded to the size of the
     largest tensor along each axis.
     """
-    if type(props[0]) is not torch.Tensor:
+    if not torch.is_tensor(props[0]):
         return torch.tensor(props)
     elif props[0].dim() == 0:
         return torch.stack(props)
@@ -16,7 +16,7 @@ def drop_zeros(props, to_keep):
     """
     Function to drop zeros from batches when the entire dataset is padded to the largest molecule size.
     """
-    if type(props[0]) is not torch.Tensor:
+    if not torch.is_tensor(props[0]):
         return props
     elif props[0].dim() == 0:
         return props

src/cormorant/data/dataset.py
@@ -13,6 +13,7 @@ class MolecularDataset(Dataset):
     """
     def __init__(self, data, included_species=None, num_pts=-1, normalize=True, shuffle=True):
 
+
         self.data = data
 
         if num_pts < 0:
@@ -24,8 +25,11 @@ class MolecularDataset(Dataset):
             else:
                 self.num_pts = num_pts
 
+        # If included species is not specified
         if included_species is None:
-            included_species = torch.unique(self.data['charges'])
+            included_species = torch.unique(self.data['charges'], sorted=True)
+            if included_species[0] == 1:
+                included_species = included_species[1:]
 
         self.included_species = included_species
 

src/cormorant/tests/cormorant_tests.py
@@ -46,32 +46,33 @@ def covariance_test(model, data):
 		covariance_test_mean = [torch.tensor(p) for p in covariance_test_mean]
 
 
-	logging.info('Rotation Invariance test:', invariance_test)
-	logging.info('Rotation Covariance test (norm):', covariance_test_norm)
-	logging.info('Rotation Covariance test (mean):', covariance_test_mean)
+	logging.info('Rotation Invariance test: {}'.format(invariance_test))
+	logging.info('Rotation Covariance test (norm): {}'.format(covariance_test_norm))
+	logging.info('Rotation Covariance test (mean): {}'.format(covariance_test_mean))
 
 
 def permutation_test(model, data):
 	logging.info('Beginning permutation test!')
 
-	mask = data['charges'] > 0
-	perm = 1*torch.arange(mask.shape[1]).expand(mask.shape[0], -1)
+	mask = data['atom_mask']
+
+	perm = torch.arange(mask.shape[1]).expand(mask.shape[0], -1)
 	for idx in range(len(mask)):
-		num_atoms = mask[idx, :].sum()
+		num_atoms = (mask[idx, :].long()).sum()
 		perm[idx, :num_atoms] = torch.randperm(num_atoms)
 	apply_perm = lambda mat: torch.stack([mat[idx, p] for (idx, p) in enumerate(perm)])
 
 	assert((mask == apply_perm(mask)).all())
 
 	data_noperm = data
-	data_perm = {key: apply_perm(val) if type(val) is torch.Tensor else None for key, val in data.items()}
+	data_perm = {key: apply_perm(val) if torch.is_tensor(val) and val.dim() > 1 else val for key, val in data.items()}
 
 	outputs_perm = model(data_perm, covariance_test=False)
 	outputs_noperm = model(data_noperm, covariance_test=False)
 
 	invariance_test = (outputs_perm - outputs_noperm).norm()
 
-	logging.info('Permutation Invariance test:', invariance_test)
+	logging.info('Permutation Invariance test: {}'.format(invariance_test))
 
 
 def cormorant_tests(model, dataloader, args, tests=['covariance'], charge_scale=1):
@@ -87,6 +88,6 @@ def cormorant_tests(model, dataloader, args, tests=['covariance'], charge_scale=
 	data = next(iter(dataloader))
 
 	covariance_test(model, data)
-	# permutation_test(model, data)
+	permutation_test(model, data)
 
 	logging.info('Test complete!')