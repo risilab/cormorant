Slight fix to covariance test to ensure doubles are used.

src/cormorant/cg_lib/rotations.py
@@ -2,9 +2,9 @@ import torch
 import numpy as np
 
 # 3D cartesian rotation matrices
-Rx = lambda theta: torch.Tensor([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]])
-Ry = lambda theta: torch.Tensor([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta),0,np.cos(theta)]])
-Rz = lambda theta: torch.Tensor([[np.cos(theta),-np.sin(theta),0],[np.sin(theta),np.cos(theta),0],[0,0,1]])
+Rx = lambda theta: torch.tensor([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]], dtype=torch.double)
+Ry = lambda theta: torch.tensor([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta),0,np.cos(theta)]], dtype=torch.double)
+Rz = lambda theta: torch.tensor([[np.cos(theta),-np.sin(theta),0],[np.sin(theta),np.cos(theta),0],[0,0,1]], dtype=torch.double)
 EulerRot = lambda alpha, beta, gamma: Rz(alpha) @ Ry(beta) @ Rz(gamma)
 
 

src/cormorant/tests/test_cg_product.py
@@ -99,7 +99,7 @@ class TestCGProduct():
 
 def _gen_rot(angles, maxl):
     alpha, beta, gamma = angles
-    D = rot.WignerD_list(maxl, alpha, beta, gamma)
+    D = rot.WignerD_list(maxl, alpha, beta, gamma, dtype=torch.double)
     R = rot.EulerRot(alpha, beta, gamma)
 
     return D, R