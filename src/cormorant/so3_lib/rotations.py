Removed SO3WignerD from rotations to avoid circular inport error. Fix later. Spherical harmonic covariance test fails.

src/cormorant/so3_lib/rotations.py
@@ -1,9 +1,9 @@
 import torch
 import numpy as np
 
-from cormorant.so3_lib import so3_wigner_d
-
-SO3WignerD = so3_wigner_d.SO3WignerD
+# from cormorant.so3_lib import so3_wigner_d
+#
+# SO3WignerD = so3_wigner_d.SO3WignerD
 
 # TODO: Update legacy code to use SO3Vec/SO3WignerD interfaces
 # TODO: Convert to PyTorch objects to allow for GPU parallelism and autograd support
@@ -15,7 +15,7 @@ Rz = lambda theta: torch.tensor([[np.cos(theta),-np.sin(theta),0],[np.sin(theta)
 EulerRot = lambda alpha, beta, gamma: Rz(alpha) @ Ry(beta) @ Rz(gamma)
 
 
-def gen_rot(maxl, angles=None):
+def gen_rot(maxl, angles=None, device=None, dtype=None):
 	"""
 	Generate a rotation matrix corresponding to a Cartesian and also a Wigner-D
 	representation of a specific  rotation. If `angles` is :obj:`None`, will
@@ -27,22 +27,38 @@ def gen_rot(maxl, angles=None):
 		Maximum weight to include in the Wigner D-matrix list
 	angles : :obj:`list` of :obj:`float` or compatible
 		Three Euler angles (alpha, beta, gamma) to parametrize the rotation.
+
+	device : :obj:`torch.device`
+		Device of the output tensor
+	dtype : :obj:`torch.dtype`
+		Data dype of the output tensor
+
+	Returns
+	-------
+	D : :obj:`list` of :obj:`torch.Tensor`
+		List of Wigner D-matrices from `l=0` to `l=maxl`
+	R : :obj:`torch.Tensor`
+		Rotation matrix that will perform the same cartesian rotation
+	angles : :obj:`tuple`
+		Euler angles that defines the input rotation
 	"""
+	# TODO : Output D as SO3WignerD
 	if angles is None:
 		alpha, beta, gamma = np.random.rand(3) * 2*np.pi
 		beta = beta / 2
+		angles = alpha, beta, gamma
 	else:
 		assert len(angles) == 3
 		alpha, beta, gamma = angles
-	D = WignerD_list(maxl, alpha, beta, gamma)
-	R = EulerRot(alpha, beta, gamma)
+	D = WignerD_list(maxl, alpha, beta, gamma, device=device, dtype=dtype)
+	R = EulerRot(alpha, beta, gamma).to(device=device, dtype=dtype)
 
-	return SO3WignerD(D), R, angles
+	return D, R, angles
 
 
 def rotate_cart_vec(R, vec):
 	""" Rotate a Cartesian vector by a Euler rotation matrix. """
-	return torch.matmul(vec, R) # Broadcast multiplication along last axis.
+	return torch.einsum('ij,...j->...i', R, vec) # Broadcast multiplication along last axis.
 
 
 def rotate_part(D, z):

tests/covariance/test_rotations.py
@@ -24,12 +24,14 @@ class TestRotations():
 
 
     @pytest.mark.parametrize('maxl', range(3))
-    @pytest.mark.parametrize('channels', range(3))
+    @pytest.mark.parametrize('channels', range(1, 3))
     def test_spherical_harmonics(self, channels, maxl):
-        D, R, angles = rot.gen_rot(maxl)
+        D, R, angles = rot.gen_rot(maxl, dtype=torch.double)
+        # D = [rot.dagger(d) for d in D]
+        D = SO3WignerD(D)
 
-        pos = torch.tensor((channels, 3), dtype=torch.double)
-        posr = rot.rotate_cart_vec(pos, R)
+        pos = torch.randn((channels, 3), dtype=torch.double)
+        posr = rot.rotate_cart_vec(R, pos)
 
         cg_dict = CGDict(maxl, dtype=torch.double)
 
@@ -37,3 +39,8 @@ class TestRotations():
         sph_harmsr = spherical_harmonics(cg_dict, posr, maxl)
 
         sph_harmsd = so3_torch.apply_wigner(sph_harms, D)
+
+        # diff = (sph_harmsr - sph_harmsd).abs()
+        diff = [(p1 - p2).abs().max() for p1, p2 in zip(sph_harmsr, sph_harmsd)]
+        print(diff)
+        assert all([d < 1e-6 for d in diff])

tests/so3_lib/test_SO3WignerD.py
@@ -8,7 +8,7 @@ class TestSO3WignerD():
 
     @pytest.mark.parametrize('maxl', range(3))
     def test_SO3WignerD_init(self, maxl):
-        data = [torch.rand((2*l+1, 2*l+1, 2) for l in range(maxl+1))]
+        data = [torch.rand((2*l+1, 2*l+1, 2)) for l in range(maxl+1)]
 
         test_wigner_d = SO3WignerD(data)
 
@@ -17,3 +17,21 @@ class TestSO3WignerD():
     def test_SO3WignerD_euler(self, maxl):
 
         test_wigner_d = SO3WignerD.euler(maxl)
+
+    @pytest.mark.parametrize('maxl', range(3))
+    def test_SO3WignerD_unitary(self, maxl):
+
+        D = SO3WignerD.euler(maxl)
+
+        Dp = SO3WignerD([rot.dagger(d) for d in D])
+
+        for d1, d2 in zip(D, Dp):
+            d1r, d1i = d1.unbind(-1)
+            d2r, d2i = d2.unbind(-1)
+
+            DDpr = torch.matmul(d1r, d2r) - torch.matmul(d1i, d2i)
+            DDpi = torch.matmul(d1i, d2r) + torch.matmul(d1r, d2i)
+
+            eye = torch.eye(DDpr.shape[0], dtype=DDpr.dtype)
+            assert (DDpr - eye).abs().max() < 1e-6
+            assert DDpi.abs().max() < 1e-6