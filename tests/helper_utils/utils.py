42725210b4f7c8ad2c9345a8c4ce029753abf656
Erik Henning Thiede <ehthiede@gmail.com>
2019-09-16 14:19:02 -0500

    Actually added the folder.  Derp

--- tests/helper_utils/utils.py
+++ tests/helper_utils/utils.py
@@ -1,10 +1,5 @@
 import torch
-import pytest
-
 from torch.utils.data import DataLoader
-
-from cormorant.models import Cormorant
-from cormorant.data import initialize_datasets, collate_fn
 
 
 class ArgsPlaceholder:
@@ -16,7 +11,9 @@ class ArgsPlaceholder:
     shuffle = False
     num_workers = 1
 
+
 def get_dataset():
+    from cormorant.data import initialize_datasets
     args = ArgsPlaceholder()
     # Initialize dataloder
     datadir = './data/'
@@ -24,20 +21,37 @@ def get_dataset():
     subset = None
 
     data_init = initialize_datasets(args, datadir, dataset, subset=subset,
-                            force_download=False, subtract_thermo=True)
+                                    force_download=False, subtract_thermo=True)
 
     args, datasets, num_species, charge_scale = data_init
 
     return args, datasets, num_species, charge_scale
 
+
 def get_dataloader():
+    from cormorant.data import collate_fn
     args, datasets, num_species, charge_scale = get_dataset()
 
     # Construct PyTorch dataloaders from datasets
     dataloader = DataLoader(datasets['train'],
-                             batch_size=args.batch_size,
-                             shuffle=args.shuffle,
-                             num_workers=args.num_workers,
-                             collate_fn=collate_fn)
+                            batch_size=args.batch_size,
+                            shuffle=args.shuffle,
+                            num_workers=args.num_workers,
+                            collate_fn=collate_fn)
 
     return dataloader, num_species, charge_scale
+
+
+def complex_from_numpy(z, dtype=torch.float, device=torch.device('cpu')):
+    """ Take a numpy array and output a complex array of the same size. """
+    zr = torch.from_numpy(z.real).to(dtype=dtype, device=device)
+    zi = torch.from_numpy(z.imag).to(dtype=dtype, device=device)
+
+    return torch.stack((zr, zi), -1)
+
+
+def numpy_from_complex(z):
+    """ Take a a complex array and return the commensurate numpy array. """
+    zr = torch.numpy(z[..., 0])
+    zi = torch.numpy(z[..., 0])
+    return zr + 1j * zi
