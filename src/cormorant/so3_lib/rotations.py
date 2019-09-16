Updated setup files, and am in the middle of debugging the covariance bug

src/cormorant/so3_lib/rotations.py
@@ -8,159 +8,247 @@ import numpy as np
 # TODO: Update legacy code to use SO3Vec/SO3WignerD interfaces
 # TODO: Convert to PyTorch objects to allow for GPU parallelism and autograd support
 
-# Explicitly construct 3D cartesian rotation matrices
-Rx = lambda theta: torch.tensor([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0,np.sin(theta),np.cos(theta)]], dtype=torch.double)
-Ry = lambda theta: torch.tensor([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta),0,np.cos(theta)]], dtype=torch.double)
-Rz = lambda theta: torch.tensor([[np.cos(theta),-np.sin(theta),0],[np.sin(theta),np.cos(theta),0],[0,0,1]], dtype=torch.double)
-EulerRot = lambda alpha, beta, gamma: Rz(alpha) @ Ry(beta) @ Rz(gamma)
+# Explicitly construct functions for the 3D cartesian rotation matrices
+
+
+# def Rx(theta):
+#     """
+#     Rotation Matrix for rotations on the x axis.
+#
+#     Parameters
+#     ----------
+#     theta : double
+#         Angle over which to rotate.
+#
+#     Returns
+#     -------
+#     Rmat : :obj:`torch.Tensor`
+#         The associated rotation matrix.
+#     """
+#     return torch.tensor([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]], dtype=torch.double)
+
+
+# def Ry(theta, device=None, dtype=None):
+def Ry(theta):
+    """
+    Rotation Matrix for rotations on the y axis.
+
+    Parameters
+    ----------
+    theta : double
+        Angle over which to rotate.
+
+    Returns
+    -------
+    Rmat : :obj:`torch.Tensor`
+        The associated rotation matrix.
+    """
+    return torch.tensor([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]], dtype=torch.double)
+
+
+def Rz(theta):
+    """
+    Rotation Matrix for rotations on the z axis. Syntax is the same as with Ry.
+    """
+    return torch.tensor([[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]], dtype=torch.double)
+
+
+def EulerRot(alpha, beta, gamma):
+    """
+    Constructs a Rotation Matrix from Euler angles.
+
+    Parameters
+    ----------
+    alpha : double
+        First Euler angle
+    beta : double
+        Second Euler angle
+    gamma : double
+        Third Euler angle
+
+    Returns
+    -------
+    Rmat : :obj:`torch.Tensor`
+        The associated rotation matrix.
+    """
+    return Rz(alpha) @ Ry(beta) @ Rz(gamma)
 
 
 def gen_rot(maxl, angles=None, device=None, dtype=None):
-	"""
-	Generate a rotation matrix corresponding to a Cartesian and also a Wigner-D
-	representation of a specific  rotation. If `angles` is :obj:`None`, will
-	generate random rotation.
+    """
+    Generate a rotation matrix corresponding to a Cartesian and also a Wigner-D
+    representation of a specific  rotation. If `angles` is :obj:`None`, will
+    generate random rotation.
 
-	Parameters
-	----------
-	maxl : :obj:`int`
-		Maximum weight to include in the Wigner D-matrix list
-	angles : :obj:`list` of :obj:`float` or compatible
-		Three Euler angles (alpha, beta, gamma) to parametrize the rotation.
+    Parameters
+    ----------
+    maxl : :obj:`int`
+        Maximum weight to include in the Wigner D-matrix list
+    angles : :obj:`list` of :obj:`float` or compatible, optional
+        Three Euler angles (alpha, beta, gamma) to parametrize the rotation.
+    device : :obj:`torch.device`, optional
+        Device of the output tensor
+    dtype : :obj:`torch.dtype`, optional
+        Data dype of the output tensor
 
-	device : :obj:`torch.device`
-		Device of the output tensor
-	dtype : :obj:`torch.dtype`
-		Data dype of the output tensor
+    Returns
+    -------
+    D : :obj:`list` of :obj:`torch.Tensor`
+        List of Wigner D-matrices from `l=0` to `l=maxl`
+    R : :obj:`torch.Tensor`
+        Rotation matrix that will perform the same cartesian rotation
+    angles : :obj:`tuple`
+        Euler angles that defines the input rotation
+    """
+    # TODO : Output D as SO3WignerD
+    if angles is None:
+        alpha, beta, gamma = np.random.rand(3) * 2*np.pi
+        beta = beta / 2
+        angles = alpha, beta, gamma
+    else:
+        assert len(angles) == 3
+        alpha, beta, gamma = angles
+    D = WignerD_list(maxl, alpha, beta, gamma, device=device, dtype=dtype)
+    # R = EulerRot(alpha, beta, gamma, device=device, dtype=dtype)
+    R = EulerRot(alpha, beta, gamma).to(device=device, dtype=dtype)
 
-	Returns
-	-------
-	D : :obj:`list` of :obj:`torch.Tensor`
-		List of Wigner D-matrices from `l=0` to `l=maxl`
-	R : :obj:`torch.Tensor`
-		Rotation matrix that will perform the same cartesian rotation
-	angles : :obj:`tuple`
-		Euler angles that defines the input rotation
-	"""
-	# TODO : Output D as SO3WignerD
-	if angles is None:
-		alpha, beta, gamma = np.random.rand(3) * 2*np.pi
-		beta = beta / 2
-		angles = alpha, beta, gamma
-	else:
-		assert len(angles) == 3
-		alpha, beta, gamma = angles
-	D = WignerD_list(maxl, alpha, beta, gamma, device=device, dtype=dtype)
-	R = EulerRot(alpha, beta, gamma).to(device=device, dtype=dtype)
-
-	return D, R, angles
+    return D, R, angles
 
 
 def rotate_cart_vec(R, vec):
-	""" Rotate a Cartesian vector by a Euler rotation matrix. """
-	return torch.einsum('ij,...j->...i', R, vec) # Broadcast multiplication along last axis.
+    """ Rotate a Cartesian vector by a Euler rotation matrix. """
+    return torch.einsum('ij,...j->...i', R, vec)  # Broadcast multiplication along last axis.
 
 
 def rotate_part(D, z):
-	""" Apply a WignerD matrix using complex broadcast matrix multiplication. """
-	Dr, Di = D.unbind(-1)
-	zr, zi = z.unbind(-1)
+    """ Apply a WignerD matrix using complex broadcast matrix multiplication. """
+    Dr, Di = D.unbind(-1)
+    zr, zi = z.unbind(-1)
 
-	matmul = lambda D, z : torch.einsum('ij,...kj->...ki', D, z)
+    matmul = lambda D, z: torch.einsum('ij,...kj->...ki', D, z)
 
-	return torch.stack((matmul(Dr, zr) - matmul(Di, zi),
-						matmul(Di, zr) + matmul(Dr, zi)), -1)
+    return torch.stack((matmul(Dr, zr) - matmul(Di, zi),
+                        matmul(Di, zr) + matmul(Dr, zi)), -1)
 
 
 def rotate_rep(D_list, rep):
-	""" Apply a WignerD rotation part-wise to a representation. """
-	ls = [(part.shape[-2]-1)//2 for part in rep]
-	D_maxls = (D_list[-1].shape[-2]-1)//2
-	assert((D_maxls >= max(ls))), 'Must have at least one D matrix for each rep! {} {}'.format(D_maxls, len(rep))
+    """ Apply a WignerD rotation part-wise to a representation. """
+    ls = [(part.shape[-2]-1)//2 for part in rep]
+    D_maxls = (D_list[-1].shape[-2]-1)//2
+    assert((D_maxls >= max(ls))), 'Must have at least one D matrix for each rep! {} {}'.format(D_maxls, len(rep))
 
-	D_list = [D_list[l] for l in ls]
-	return [rotate_part(D, part) for (D, part) in zip(D_list, rep)]
+    D_list = [D_list[l] for l in ls]
+    return [rotate_part(D, part) for (D, part) in zip(D_list, rep)]
 
 
 def dagger(D):
-	conj = torch.tensor([1, -1], dtype=D.dtype, device=D.device).view(1, 1, 2)
-	D = (D*conj).permute((1, 0, 2))
-	return D
+    conj = torch.tensor([1, -1], dtype=D.dtype, device=D.device).view(1, 1, 2)
+    D = (D*conj).permute((1, 0, 2))
+    return D
 
 
 def create_J(j):
-	mrange = -np.arange(-j, j)
-	jp_diag = np.sqrt((j+mrange)*(j-mrange+1))
-	Jp = np.diag(jp_diag, k=1)
-	Jm = np.diag(jp_diag, k=-1)
+    mrange = -np.arange(-j, j)
+    jp_diag = np.sqrt((j+mrange)*(j-mrange+1))
+    Jp = np.diag(jp_diag, k=1)
+    Jm = np.diag(jp_diag, k=-1)
 
-	# Jx = (Jp + Jm) / complex(2, 0)
-	# Jy = -(Jp - Jm) / complex(0, 2)
-	Jz = np.diag(-np.arange(-j, j+1))
-	Id = np.eye(2*j+1)
+    # Jx = (Jp + Jm) / complex(2, 0)
+    # Jy = -(Jp - Jm) / complex(0, 2)
+    Jz = np.diag(-np.arange(-j, j+1))
+    Id = np.eye(2*j+1)
 
-	return Jp, Jm, Jz, Id
+    return Jp, Jm, Jz, Id
 
 
 def create_Jy(j):
-	mrange = -np.arange(-j, j)
-	jp_diag = np.sqrt((j+mrange)*(j-mrange+1))
-	Jp = np.diag(jp_diag, k=1)
-	Jm = np.diag(jp_diag, k=-1)
+    mrange = -np.arange(-j, j)
+    jp_diag = np.sqrt((j+mrange)*(j-mrange+1))
+    Jp = np.diag(jp_diag, k=1)
+    Jm = np.diag(jp_diag, k=-1)
 
-	Jy = -(Jp - Jm) / complex(0, 2)
+    Jy = -(Jp - Jm) / complex(0, 2)
 
-	return Jy
+    return Jy
 
 
 def create_Jx(j):
-	mrange = -np.arange(-j, j)
-	jp_diag = np.sqrt((j+mrange)*(j-mrange+1))
-	Jp = np.diag(jp_diag, k=1)
-	Jm = np.diag(jp_diag, k=-1)
+    mrange = -np.arange(-j, j)
+    jp_diag = np.sqrt((j+mrange)*(j-mrange+1))
+    Jp = np.diag(jp_diag, k=1)
+    Jm = np.diag(jp_diag, k=-1)
 
-	Jx = (Jp + Jm) / complex(2, 0)
+    Jx = (Jp + Jm) / complex(2, 0)
 
-	return Jx
+    return Jx
 
 
 def littled(j, beta):
-	Jy = create_Jy(j)
+    Jy = create_Jy(j)
 
-	evals, evecs = np.linalg.eigh(Jy)
-	evecsh = evecs.conj().T
-	evals_exp = np.diag(np.exp(complex(0, -beta)*evals))
+    evals, evecs = np.linalg.eigh(Jy)
+    evecsh = evecs.conj().T
+    evals_exp = np.diag(np.exp(complex(0, -beta)*evals))
 
-	d = np.matmul(np.matmul(evecs, evals_exp), evecsh)
+    d = np.matmul(np.matmul(evecs, evals_exp), evecsh)
 
-	return d
+    return d
 
 
 def WignerD(j, alpha, beta, gamma, numpy_test=False, dtype=torch.float, device=torch.device('cpu')):
-	d = littled(j, beta)
+    """
+    Calculates the Wigner D matrix for a given degree and Euler Angle.
 
-	Jz = np.arange(-j, j+1)
-	Jzl = np.expand_dims(Jz, 1)
+    Parameters
+    ----------
+    j : int
+        Degree of the representation.
+    alpha : double
+        First Euler angle
+    beta : double
+        Second Euler angle
+    gamma : double
+        Third Euler angle
+    numpy_test : bool, optional
+        ?????
+    device : :obj:`torch.device`, optional
+        Device of the output tensor
+    dtype : :obj:`torch.dtype`, optional
+        Data dype of the output tensor
 
-	# np.multiply() broadcasts, so this isn't actually matrix multiplication, and 'left'/'right' are lies
-	left = np.exp(complex(0, -alpha)*Jzl)
-	right = np.exp(complex(0, -gamma)*Jz)
+    Returns
+    -------
+    D = 
 
-	D = left * d * right
 
-	if not numpy_test:
-		D = complex_from_numpy(D, dtype=dtype, device=device)
+    """
+    d = littled(j, beta)
 
-	return D
+    Jz = np.arange(-j, j+1)
+    Jzl = np.expand_dims(Jz, 1)
+
+    # np.multiply() broadcasts, so this isn't actually matrix multiplication, and 'left'/'right' are lies
+    left = np.exp(complex(0, -alpha)*Jzl)
+    right = np.exp(complex(0, -gamma)*Jz)
+
+    D = left * d * right
+
+    if not numpy_test:
+        D = complex_from_numpy(D, dtype=dtype, device=device)
+
+    return D
 
 
 def WignerD_list(jmax, alpha, beta, gamma, numpy_test=False, dtype=torch.float, device=torch.device('cpu')):
-	return [WignerD(j, alpha, beta, gamma, numpy_test=numpy_test, dtype=dtype, device=device) for j in range(jmax+1)]
+    """
+
+    """
+    return [WignerD(j, alpha, beta, gamma, numpy_test=numpy_test, dtype=dtype, device=device) for j in range(jmax+1)]
 
 
 def complex_from_numpy(z, dtype=torch.float, device=torch.device('cpu')):
-	""" Take a numpy array and output a complex array of the same size. """
-	zr = torch.from_numpy(z.real).to(dtype=dtype, device=device)
-	zi = torch.from_numpy(z.imag).to(dtype=dtype, device=device)
+    """ Take a numpy array and output a complex array of the same size. """
+    zr = torch.from_numpy(z.real).to(dtype=dtype, device=device)
+    zi = torch.from_numpy(z.imag).to(dtype=dtype, device=device)
 
-	return torch.stack((zr, zi), -1)
+    return torch.stack((zr, zi), -1)

tests/cg_lib/test_spherical_harmonics.py
@@ -9,11 +9,12 @@ from utils import complex_from_numpy
 
 # Test cg_product runs and aggregate=True works
 
+
 class TestSphericalHarmonics():
 
     # Compare with SciyPy
     @pytest.mark.parametrize('maxl', range(3))
-    @pytest.mark.parametrize('batch', [(1,), (2,), (5,), (1,1), (2,1), (1,2), (1, 1, 1), (2, 2, 2), (2, 3, 3)])
+    @pytest.mark.parametrize('batch', [(1,), (2,), (5,), (1, 1), (2, 1), (1, 2), (1, 1, 1), (2, 2, 2), (2, 3, 3)])
     def test_spherical_harmonics_vs_scipy(self, maxl, batch):
         cg_dict = CGDict(maxl=maxl, dtype=torch.double)
 
@@ -28,7 +29,7 @@ class TestSphericalHarmonics():
 
     # Compare with SciyPy
     @pytest.mark.parametrize('maxl', range(3))
-    @pytest.mark.parametrize('batch', [(1,), (2,), (5,), (1,1), (2,1), (1,2), (1, 1, 1), (2, 2, 2), (2, 3, 3)])
+    @pytest.mark.parametrize('batch', [(1,), (2,), (5,), (1, 1), (2, 1), (1, 2), (1, 1, 1), (2, 2, 2), (2, 3, 3)])
     @pytest.mark.parametrize('natoms1', [1, 2, 5])
     @pytest.mark.parametrize('natoms2', [1, 2, 5])
     def test_spherical_rel_harmonics_vs_scipy(self, maxl, batch, natoms1, natoms2):
@@ -42,10 +43,12 @@ class TestSphericalHarmonics():
         sh_sp, norms_sp = sph_harms_rel_from_scipy(pos1, pos2, maxl)
 
         for l, (part1, part2) in enumerate(zip(sh, sh_sp)):
-            if l == 0: continue
+            if l == 0:
+                continue
             assert torch.allclose(part1, part2)
 
         assert torch.allclose(norms, norms_sp)
+
 
 def sph_harms_rel_from_scipy(pos1, pos2, maxl):
     """ Calculate the relative spherical harmonics using SciPy's special function reoutine """
@@ -69,7 +72,8 @@ def sph_harms_rel_from_scipy(pos1, pos2, maxl):
             for aidx2 in range(s12[2]):
                 vec1 = pos1[bidx, aidx1]
                 vec2 = pos2[bidx, aidx2]
-                if (vec1 == vec2).all(): continue
+                if (vec1 == vec2).all():
+                    continue
                 sh_temp = sph_harms_from_scipy(vec1-vec2, maxl)
                 norms[bidx, aidx1, aidx2] = (vec1-vec2).norm()
                 for l in range(maxl + 1):
@@ -83,7 +87,7 @@ def sph_harms_rel_from_scipy(pos1, pos2, maxl):
 
 
 def sph_harms_from_scipy(pos, maxl):
-    """ Calculate the spherical harmonics using SciPy's special function reoutine """
+    """ Calculate the spherical harmonics using SciPy's special function routine """
     s = pos.shape
 
     pos = pos.view(-1, 3)

tests/covariance/test_rotations.py
@@ -3,8 +3,8 @@ import pytest
 
 from cormorant.so3_lib import so3_torch, SO3Tau, SO3Vec, SO3WignerD
 from cormorant.so3_lib import rotations as rot
-
 from cormorant.cg_lib import spherical_harmonics, CGDict
+from utils import numpy_from_complex, complex_from_numpy
 
 
 class TestRotations():
@@ -21,21 +21,62 @@ class TestRotations():
     @pytest.mark.parametrize('maxl', range(3))
     @pytest.mark.parametrize('channels', range(1, 3))
     def test_spherical_harmonics(self, channels, maxl):
-        D, R, angles = rot.gen_rot(maxl, dtype=torch.double)
-        # D = [rot.dagger(d) for d in D]
+        # D, R, angles = rot.gen_rot(maxl, dtype=torch.double)
+        import numpy as np
+        D, R, angles = rot.gen_rot(maxl, angles=[np.pi/2, 0, 0], dtype=torch.double)
         D = SO3WignerD(D)
+        D_numpy = np.round(numpy_from_complex(D[1]), decimals=8)
 
-        pos = torch.randn((channels, 3), dtype=torch.double)
+        # pos = torch.randn((channels, 3), dtype=torch.double)
+        pos = torch.eye(3, dtype=torch.double)
         posr = rot.rotate_cart_vec(R, pos)
 
         cg_dict = CGDict(maxl, dtype=torch.double)
 
-        sph_harms = spherical_harmonics(cg_dict, pos, maxl)
-        sph_harmsr = spherical_harmonics(cg_dict, posr, maxl)
+        sph_harms = spherical_harmonics(cg_dict, pos, maxl, sh_norm='qm')
+        scipy_sph_harms_1 = sph_harms_from_scipy(pos, maxl)[1]
+        print('original')
+        print(sph_harms[1])
+        print(sph_harms.channels, 'channels')
+        print('Scipy:')
+        print(scipy_sph_harms_1)
+        print('-----')
+        sph_harmsr = spherical_harmonics(cg_dict, posr, maxl, sh_norm='qm')
+        scipy_sph_harms_1_r = sph_harms_from_scipy(posr, maxl)[1]
+        print('rotated')
+        print(sph_harmsr[1])
+        print(scipy_sph_harms_1_r)
 
         sph_harmsd = so3_torch.apply_wigner(sph_harms, D)
+        print('-----')
+        print('rotated')
+        print(np.round(numpy_from_complex(sph_harmsd), decimals=8))
+        print('scipy')
+        print(D_numpy.dot(scipy_sph_harms_1))
 
         # diff = (sph_harmsr - sph_harmsd).abs()
         diff = [(p1 - p2).abs().max() for p1, p2 in zip(sph_harmsr, sph_harmsd)]
         print(diff)
         assert all([d < 1e-6 for d in diff])
+
+
+def sph_harms_from_scipy(pos, maxl):
+    """ Calculate the spherical harmonics using SciPy's special function routine """
+    import numpy as np
+    from scipy.special import sph_harm as scipy_sph_harm
+    s = pos.shape
+
+    pos = pos.view(-1, 3)
+    norm = pos.norm(dim=-1).unsqueeze(-1)
+
+    x, y, z = (pos/norm).unbind(-1)
+    phi = torch.atan2(y, x)
+    theta = torch.acos(z)
+
+    phi, theta = phi.view(-1, 1).numpy(), theta.view(-1, 1).numpy()
+
+    sph_harms = []
+    for l in range(maxl + 1):
+        sph_harm_l = scipy_sph_harm(np.arange(-l, l+1).reshape(1, -1), l, phi, theta)
+        sph_harms.append(sph_harm_l)
+    return sph_harms

tests/helper_utils/utils.py
@@ -52,6 +52,6 @@ def complex_from_numpy(z, dtype=torch.float, device=torch.device('cpu')):
 
 def numpy_from_complex(z):
     """ Take a a complex array and return the commensurate numpy array. """
-    zr = torch.numpy(z[..., 0])
-    zi = torch.numpy(z[..., 0])
+    zr = (z[..., 0]).numpy()
+    zi = (z[..., 1]).numpy()
     return zr + 1j * zi